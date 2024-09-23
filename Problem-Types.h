b15cb860a06b47dd795d5541a5c9a7358c9732cf
Leonid Sokolinsky <29542960+leonid-sokolinsky@users.noreply.github.com>
2024-09-23 10:39:00 +0500

    Add files via upload
    The MPS format converter has been added. To enable it, use #define PP_MPS_FORMAT. The MPS format must be used for all problems from the Netlib-LP repository.

--- Problem-Types.h
+++ Problem-Types.h
@@ -12,4 +12,24 @@ This source code has been produced with using BSF-skeleton
 //=========================== Problem Types =========================
 typedef double PT_matrix_T[PP_MM][PP_N];
 typedef double PT_vector_T[PP_N];
-typedef double	PT_column_T[PP_MM];
+typedef double PT_column_T[PP_MM];
+
+typedef char PT_MPS_name_T[9];
+
+struct PT_MPS_row_T {
+	PT_MPS_name_T name;
+	char type;
+	double RHS_value;
+};
+
+struct PT_MPS_column_T {
+	PT_MPS_name_T varName;
+	PT_MPS_name_T rowName;
+	double value;
+	int j;
+};
+
+struct PT_MPS_upBound_T {
+	int varIndex;
+	double value;
+};
